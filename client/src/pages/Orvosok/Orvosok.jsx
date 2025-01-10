4b8e918063c06a3c46d57c7a1348743a1f52071e
SzaraGitHub <demeter.ricsi2005@gmail.com>
2025-01-10 12:22:59 +0100

    kbkesz

--- client/src/pages/Orvosok/Orvosok.jsx
+++ client/src/pages/Orvosok/Orvosok.jsx
@@ -7,35 +7,32 @@ const Idopont = () => {
 
   useEffect(() => {
     const dolgoz = async () => {
-      // Kérés az orvosok listájára
       const response = await fetch('http://localhost:3001/doctorsfrontend');
       const valasz = await response.json();
-      console.log(valasz.doctors);
       if (response.ok) {
         setOrvosok(valasz.doctors);
       }
     };
 
     dolgoz();
-  }, []); // Az üres tömb biztosítja, hogy csak egyszer fusson le a `useEffect`
+  }, []);
 
   return (
     <div className="info-container">
-      <br />
       <div className="text-section">
         <h1>Orvosok</h1>
       </div>
-      <div>
-        <h5>Orvosok listája:</h5>
-        <div className="doctors-list">
-          {orvosok.map((doctor, index) => (
-            <div key={doctor.id || index} className="doctor-card">
+      <div className="doctors-list">
+        {orvosok.map((doctor, index) => (
+          <div key={doctor.id || index} className="doctor-card">
+            <div className="doctor-card-header">
               <h3>{doctor.nev}</h3>
-              <p><strong>Szakma:</strong> {doctor.szak}</p>
-              <br />
             </div>
-          ))}
-        </div>
+            <div className="doctor-card-body">
+              <p><strong>Szakma:</strong> {doctor.szak}</p>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
