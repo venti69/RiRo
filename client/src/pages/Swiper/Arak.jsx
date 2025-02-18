f07cc252f2eef7db4c1a8ea2573f3a8a53505dde
SzaraGitHub <demeter.ricsi2005@gmail.com>
2025-02-18 11:21:44 +0100

    arakbanvoltvalami

--- client/src/pages/Swiper/Arak.jsx
+++ client/src/pages/Swiper/Arak.jsx
@@ -3,7 +3,6 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
 import { Pagination } from "swiper/modules";
-import { FaCalendarAlt } from "react-icons/fa";
 import { AiOutlineArrowRight } from "react-icons/ai";
 
 const promotions = [
