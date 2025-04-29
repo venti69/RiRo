8c23df36a8df638cc7836e293c5bc3c9af9d7b17
venti69 <nagyvatir@gmail.com>
2025-04-29 07:24:26 +0200

    nemtudomxD

--- client/cypress/e2e/info.cy.js
+++ client/cypress/e2e/info.cy.js
@@ -3,9 +3,11 @@ describe('információs oldal tesztelése', () => {
     it('Betöltődik az információs oldal', () => {
       cy.visit('http://localhost:5173/info');
         cy.wait(2000);
-      cy.url().should('include', '/info'); // Megnézi, hogy a URL helyes-e
+      cy.url().should('include', '/info');
         cy.wait(2000);
-      cy.get('h2').should('exist'); // Megnézi, hogy van-e <h1> az oldalon
+      cy.get('h2').should('exist');
+        cy.wait(2000);
+      cy.get('summary').click();
     });
     
 
